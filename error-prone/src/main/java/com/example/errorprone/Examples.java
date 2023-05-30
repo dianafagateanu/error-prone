f0fc9c19acae61f5039a7aa348a7506d51807529
Mihaela-Diana Fagateanu <mihaela-diana.fagateanu1@ing.com>
2023-05-30 14:37:08 +0300

    add  initial version with the ArrayEquals bug pattern before patch

--- error-prone/src/main/java/com/example/errorprone/Examples.java
+++ error-prone/src/main/java/com/example/errorprone/Examples.java
@@ -4,7 +4,15 @@ import java.util.Arrays;
 
 public class Examples {
 
+    /**
+     * ArrayEquals bug pattern
+     * <br>
+     * <br>
+     * Generally when comparing arrays for equality, the programmer intends to check that the contents of the arrays are equal rather than that they are actually the same object.
+     * But many commonly used equals methods compare arrays for reference equality rather than content equality.
+    **/
+
     public boolean compare(String firstList[], String secondList[]) {
-        return Arrays.equals(firstList, secondList);
+        return firstList.equals(secondList);
     }
 }
